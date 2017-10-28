72341c71ef406fa5548e242fff6016f471a433d7
Johannes Ebner <johannes.ebner@re-flekt.com>
2017-10-29 00:05:29 +0200

    Added CRUD set of razor pages  for customers

---
+++ src/Claimini.Web/Pages/Customers/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Claimini.Web.Data;
+
+namespace Claimini.Web.Pages.Customers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Claimini.Web.Data.ApplicationDbContext _context;
+
+        public CreateModel(Claimini.Web.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Customer.Add(Customer);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
