299ec1c848e714d28a0302e3fd9364f8ec1923f2
Johannes Ebner <johannes.ebner@re-flekt.com>
2017-10-29 01:14:03 +0200

    Added documentation to the Customer class

--- src/Claimini.Web/Data/Customer.cs
+++ src/Claimini.Web/Data/Customer.cs
@@ -2,32 +2,55 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Claimini.Web.Data
 {
+    /// <summary>
+    /// Represents a Customer
+    /// </summary>
     public class Customer
     {
+        /// <summary>
+        /// The Id of the Customer
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The Name of the Customer
+        /// </summary>
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
 
+        /// <summary>
+        /// The Street Address
+        /// </summary>
         [Required]
         [StringLength(50)]
         [Display(Name = "Street Address")]
         public string StreetAddress { get; set; }
 
-
+        /// <summary>
+        /// An additional, optional address field
+        /// </summary>
         [Display(Name = "Additional Street Address")]
         [StringLength(50)]
         public string StreetAddressAdditional { get; set; }
 
+        /// <summary>
+        /// The ZIP Code
+        /// </summary>
         [Required]
         [StringLength(10)]
         [Display(Name = "Zip Code")]
         public string ZipCode { get; set; }
 
+        /// <summary>
+        /// The state in which the customer resides
+        /// </summary>
         [StringLength(20)]
         public string State { get; set; }
 
+        /// <summary>
+        /// The Country in which the Customer resides
+        /// </summary>
         [Required]
         [StringLength(30)]
         public string Country { get; set; }
