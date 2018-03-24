db12905f2752737dcc2a8edf1bd1e7d22adbc016
Johannes Ebner <Johannes.Ebner@free-hands.org>
2018-03-24 22:23:33 +0100

    added a blazor client project with a simpel customer list page

---
+++ src/Claimini.BlazorClient/Dto/Customer.cs
@@ -0,0 +1,43 @@
+namespace Claimini.BlazorClient.Dto
+{
+    /// <summary>
+    /// Represents a Customer
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// Gets or sets the Id of the Customer
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name of the Customer
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Street Address
+        /// </summary>
+        public string StreetAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets an additional, optional address field
+        /// </summary>
+        public string StreetAddressAdditional { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ZIP Code
+        /// </summary>
+        public string ZipCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the state in which the customer resides
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Country in which the Customer resides
+        /// </summary>
+        public string Country { get; set; }
+    }
+}
