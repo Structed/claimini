5e5e937aff207fdeb42e1b3bd5ef1a3107def219
Johannes Ebner <Johannes.Ebner@free-hands.org>
2018-10-28 18:02:55 +0100

    Removed obsolete lines

--- src/Claimini.BlazorClient/Program.cs
+++ src/Claimini.BlazorClient/Program.cs
@@ -1,9 +1,4 @@
-using BlazorRedux;
-using Claimini.BlazorClient.ApplicationState;
-using Microsoft.AspNetCore.Blazor.Browser.Rendering;
-using Microsoft.AspNetCore.Blazor.Browser.Services;
 using Microsoft.AspNetCore.Blazor.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Claimini.BlazorClient
 {
@@ -11,15 +6,6 @@ namespace Claimini.BlazorClient
     {
         static void Main(string[] args)
         {
-//            var serviceProvider = new BrowserServiceProvider(configure =>
-//            {
-//                // Add any custom services here
-//                configure.AddSingleton<IApiClient, ApiClient>();
-//                configure.AddReduxStore<AppState, IAction>(new AppState(), Reducers.RootReducer);
-//            });
-//
-//            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
-            
             CreateHostBuilder(args).Build().Run();
         }
         
