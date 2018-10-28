a2274af0224e9fc52db625732ef844f22c752842
Johannes Ebner <Johannes.Ebner@free-hands.org>
2018-10-28 16:45:36 +0100

    added configuration for ApiClient and Redux

--- src/Claimini.BlazorClient/Startup.cs
+++ src/Claimini.BlazorClient/Startup.cs
@@ -1,3 +1,5 @@
+using BlazorRedux;
+using Claimini.BlazorClient.ApplicationState;
 using Microsoft.AspNetCore.Blazor.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +9,8 @@ namespace Claimini.BlazorClient
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IApiClient, ApiClient>();
+            services.AddReduxStore<AppState, IAction>(new AppState(), Reducers.RootReducer);
         }
 
         public void Configure(IBlazorApplicationBuilder app)
